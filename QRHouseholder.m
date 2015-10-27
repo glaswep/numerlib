9bcef8cd06a8d5fc0546130cfa33596e08fbb792
Yulun Zeng <yz5454@gmail.com>
2015-10-27 15:14:51 -0500

    QR decomposition using Householder reflection

---
+++ QRHouseholder.m
@@ -0,0 +1,22 @@
+function [Q, R] = QRHouseholder(A)
+
+[m,n] = size(A);                            % Find size of A
+Q = eye(m);                                 % Intialize Q
+
+for k=1:min(m-1,n)
+   ak  = A(k:m,k);
+   vk  = ak; vk(1) = vk(1) + exp(1i*angle(ak(1)))*norm(ak);
+                                            % Find v for householder
+   vk  = vk/norm(vk);                       % Normalize
+    %    c = (ak'*vk)/(vk'*ak);             % Alternative c value
+   c = 1;
+   A(k:m,k:n) = A(k:m,k:n) - (1+c)*vk*(vk'*A(k:m,k:n));
+                                            % Apply Householder on subset
+                                            % of matrix A
+   Q(:,k:end) = Q(:,k:end) - (1+c)*Q(:,k:end)*(vk*vk');
+                                            % Apply Householder on subset
+                                            % of matrix Q                                           
+end
+R = A;                                      % Now A is just R
+
+end
